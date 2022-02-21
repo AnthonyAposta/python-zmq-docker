2d189765b7896b479dd5b350231b9618aa9bcc2e
anthony <anthonyjbet@gmail.com>
2022-02-21 15:29:00 -0300

    adicionando sistemas de tópicos do ZMQ

--- consumer/consumer.py
+++ consumer/consumer.py
@@ -3,14 +3,16 @@
 from email import message
 import zmq
 
+
 context = zmq.Context()
 # Socket to receive messages
 receiver = context.socket(zmq.SUB)
 receiver.connect('tcp://gateway-service:3231')
-receiver.subscribe("")
+#receiver.connect('tcp://0.0.0.0:3231')
+receiver.subscribe("crawler.crawler1")
 
 print('waiting for messages')
 while True:
 
-    message = receiver.recv_json()
+    message = receiver.recv()
     print(message)
--- crawlers/crawler.py
+++ crawlers/crawler.py
@@ -1,10 +1,20 @@
 # crawler to receive data from twitter api
 
-from email import message
 import json
+from pydoc_data import topics
 import zmq
 from time import sleep
-#import tweepy
+
+
+def message_encoder(topic_str: str, message_dict: dict) -> bytes:
+
+    message_json = json.dumps(message_dict)
+    
+    message = topic_str + ' ' + message_json
+
+    encoded_message = str.encode(message)
+
+    return encoded_message 
 
 # Creates a socket instance
 context = zmq.Context()
@@ -12,6 +22,9 @@ socket = context.socket(zmq.PUB)
 
 print("conneting to xpub")
 socket.connect("tcp://gateway-service:5001")
+#socket.connect("tcp://0.0.0.0:5001")
+
+topic = 'crawler.crawler1'
 
 # Sends a string message
 message_id = 0
@@ -19,7 +32,6 @@ while True:
 
     message_id += 1
     message = {"message": "this is a message", "id": message_id}
-    message_json = json.dumps(message)
-    socket.send_json(message_json)
-    print(message_json)
+    socket.send( message_encoder(topic, message) )
+    print(message)
     sleep(1)
